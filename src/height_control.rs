45bb90a4a560ed2fbe04ea10dc77c5e2ba817011
Julian Bayardo <julian@bayardo.info>
2017-07-27 15:04:57 -0300

    Document height_control.rs and skiplist.rs

--- src/height_control.rs
+++ src/height_control.rs
@@ -6,17 +6,89 @@ use std::default::Default;
 extern crate rand;
 use self::rand::{random, Open01};
 
+/// This trait is for structures that implement a height generation strategy for
+/// `SkipList<K>`.
+///
+/// Types that implement this trait are expected to be used as arguments to
+/// `SkipList<K>::new`, and are responsible for returning the height to be used
+/// for any given element that will be inserted in the Skip List.
+///
+/// Users should avoid implementing this trait unless there are effectively
+/// space or speed concerns and they are certain that a change in the strategy
+/// will fix their problem.
 pub trait HeightControl<K> {
+	/// Returns the maximum height that this controller can generate. This value
+	/// must be constant per instance: implementors of this trait should not
+	/// allow the output of this function to ever change after an instance has
+	/// been created.
+	/// 
+	/// # Remarks
+	/// 
+	/// This is used by the Skip List to decide how much space to allocate for
+	/// the head node. The impact of this value is very high: every search in
+	/// the skip list needs to allocate a vector of the size given by this
+	/// function. Searches happen in every action on it except for iteration.
+	///
+	/// It is also called very frequently, which means that it should ideally be
+	/// both inlineable and O(1).
     fn max_height(&self) -> usize;
+
+    /// Generates a height for the `key`.
+    ///
+    /// # Arguments
+    ///
+    ///  * `key`: element for which the height should be generated.
+    ///
+    /// # Remarks
+    ///
+    /// The height returned by this function will be the level of the node for
+    /// this `key`. This function does not need to be referentially transparent,
+    /// and it may or may not be non-deterministic.
+    ///
+    /// This function highly influences the runtime of all operations in the
+    /// SkipList. Exercise caution when implementing this function. A few things
+    /// to keep in mind:
+    ///
+    ///  1. It is called in every insertion. Ideally, it should be O(1).
+    ///  2. High values for this function mean more pointers between nodes and 
+    ///     bigger vectors.
+    ///  3. Nodes of a given level will all be linked between themselves, so it
+    ///     also affects the search strategy.
+    ///  4. If you are using a RNG for this function, then that means you will
+    ///     have to update the internal state whenever doing an insertion; try
+    ///     to keep these updates within control.
     fn get_height(&mut self, key: &K) -> usize;
 }
 
+/// Implements height generation through simulation of a capped geometrical
+/// random variable.
+///
+/// This implements the algorithm in the original paper:
+/// * William Pugh. 1990. "Skip lists: a probabilistic alternative to balanced
+///   trees". Commun. ACM 33, 6 (June 1990), 668-676.
+///   DOI=http://dx.doi.org/10.1145/78973.78977
+///
+/// And is only included here for completeness, `PowTwoGenerator` should always
+/// be preferred.
 pub struct GeometricalGenerator {
     upgrade_probability_: f64,
     max_height_: usize,
 }
 
 impl GeometricalGenerator {
+	/// Builds a new `GeometricalGenerator`
+	///
+	/// # Arguments
+	///
+	///  * `max_height`: maximum height that the generator may give out to any
+	///    node.
+	///  * `upgrade_probability`: the probability used when simulating the
+	///    geometrical random variable.
+	///
+	/// # Remarks
+	///
+	/// This generator uses an RNG to simulate up to `max_heights` coin throws
+	/// in every `get_height` call. This is slow, so it should be avoided.
     pub fn new(max_height: usize, upgrade_probability: f64) -> GeometricalGenerator {
         GeometricalGenerator {
             upgrade_probability_: upgrade_probability,
@@ -50,12 +122,12 @@ impl<K> HeightControl<K> for GeometricalGenerator {
     }
 }
 
-// 'HashCoinGenerator' creates heights by using a hash function that distributes
-// uniformly among the output universe and counting the number of trailing zeros
-// in the hashed value of a key. This is akin to using a Geometric(1/2) for all
-// practical purposes; however, it is faster than generating a random number for
-// an unknown amount of time, assuming that the hash function is not too
-// expensive.
+/// `HashCoinGenerator` creates heights by using a hash function that
+/// distributes uniformly among the output universe and counting the number of
+/// trailing zeros in the hashed value of a key. This is akin to using a
+/// Geometric(1/2) when assuming the insertions are uniformly random; however,
+/// it is faster than generating a random number for an unknown amount of time,
+/// assuming that the hash function is not too expensive.
 pub struct HashCoinGenerator<K, H> {
     max_height_: usize,
     hasher_: H,
@@ -63,6 +135,25 @@ pub struct HashCoinGenerator<K, H> {
 }
 
 impl<K: std::hash::Hash, H: std::hash::Hasher> HashCoinGenerator<K, H> {
+	/// Builds a new `HashCoinGenerator`
+	///
+	/// # Arguments
+	///
+	///  * `max_height`: maximum height that the generator may give out to any
+	///    node.
+	///  * `hasher`: the hash function that will be used to generate the level
+	///    for a node. This should be from at least a 2-universal family.
+	///
+	/// # Remarks
+	///
+	/// The implementation can not and does not check for 2-universality on the
+	/// hash function. A bad hash function may skew the generated heights
+	/// towards bad distribution values and, in doing so, unbalance the skip
+	/// list and affect its guarantees.
+	///
+	/// As an example, if using this generator to build `u32` keys, and the
+	/// hash function is the identity function, the `get_height` function will
+	/// entirely depend on the input distribution.
     pub fn new(max_height: usize, hasher: H) -> HashCoinGenerator<K, H> {
         HashCoinGenerator {
             max_height_: max_height,
@@ -91,47 +182,49 @@ impl<K: std::hash::Hash, H: std::hash::Hasher> HeightControl<K> for HashCoinGene
     }
 }
 
-// 'HashingGenerator' creates heights by calling a hash function and capping the
-// value at the maximum allowed.
-pub struct HashingGenerator<K, H> {
-    max_height_: usize,
-    hasher_: H,
+/// `TwoPowGenerator` generates heights by simulating a capped geometrical
+/// random variable, similar to `GeometricalGenerator`. This generator is
+/// restricted to maximum heights that are powers of two and upgrades with
+/// probability 1/2.
+/// 
+/// It should be preferred to `GeometricalGenerator` because the simulation is
+/// done using only a single random throw.
+pub struct TwoPowGenerator<K> {
+    max_pow_: usize,
     phantom_: std::marker::PhantomData<K>,
 }
 
-impl<K: std::hash::Hash, H: std::hash::Hasher> HashingGenerator<K, H> {
-    pub fn new(max_height: usize, hasher: H) -> HashingGenerator<K, H> {
-        HashingGenerator {
-            max_height_: max_height,
-            hasher_: hasher,
+impl<K> TwoPowGenerator<K> {
+    pub fn new(max_height: usize) -> TwoPowGenerator<K> {
+    	assert!(max_height.is_power_of_two());
+
+        TwoPowGenerator {
+            max_pow_: max_height - 1,
             phantom_: std::marker::PhantomData,
         }
     }
 }
 
-impl<K: std::hash::Hash, H: std::hash::Hasher> HeightControl<K> for HashingGenerator<K, H> {
+impl<K> HeightControl<K> for TwoPowGenerator<K> {
     #[inline(always)]
     fn max_height(&self) -> usize {
-        self.max_height_
+        self.max_pow_ + 1
     }
 
+    #[allow(unused_variables)]
     fn get_height(&mut self, key: &K) -> usize {
-        key.hash(&mut self.hasher_);
-        let height = self.hasher_.finish() as usize;
-        // TODO: this is biased to low end values, unless max_height_ is a power
-        // of two.
-        let output = height % self.max_height_;
-        output
+    	// The probability that a random value has a binary representation that
+    	// ends with 1 0^k is (1/2)^{k+1}.
+        let height = random::<usize>().trailing_zeros() as usize;
+        // Since we are always doing `% 2^k` here, we are using the simple trick
+        // exposed here: https://stackoverflow.com/q/6670715 .
+        height & self.max_pow_
     }
 }
 
 impl<K: 'static + std::hash::Hash + Default> Default for SkipList<K> {
-    // TODO: fix when SipHasher is no longer deprecated
-    #[allow(deprecated)]
     #[inline(always)]
     fn default() -> Self {
-        let generator 
-            = HashCoinGenerator::new(16, std::hash::SipHasher::default());
-        Self::new(Box::new(generator))
+        Self::new(Box::new(TwoPowGenerator::new(16)))
     }
 }
--- src/skiplist.rs
+++ src/skiplist.rs
@@ -4,333 +4,324 @@ use height_control::HeightControl;
 use std;
 
 pub struct SkipList<K> {
-    pub(crate) head_: *mut Node<K>,
+  /// Pointer to the head of the Skip List. The first node is actually a "ghost"
+  /// node: it is created within `SkipList::new`, should only be deleted in
+  /// `SkipList::drop`, has the maximum possible height, and it holds dummy data
+  /// that should never be touched by the algorithms.
+  ///
+  /// The reason we have the ghost node is because it simplifies the algorithms
+  /// considerably. Searches for nodes all begin in the ghost node, which has
+  /// as `next(0)` the actual first element, if any.
+  pub(crate) head_: *mut Node<K>,
 
-    length_: usize,
-    height_: usize,
-    controller_: Box<HeightControl<K>>,
+  /// Number of elements in the SkipList
+  length_: usize,
+
+  /// Maximum reached height
+  height_: usize,
+
+  /// Maximum height the `controller_` can generate. This is stored here instead
+  /// of calling `controller_` because all calls to `controller_` are virtually
+  /// dispatched, which is more expensive than just holding an usize.
+  max_height_: usize,
+
+  /// Used to generate the height for any given node when inserting data.
+  controller_: Box<HeightControl<K>>,
 }
 
 impl<K: Default> SkipList<K> {
-    pub fn new(controller: Box<HeightControl<K>>) -> SkipList<K> {
-        // This assertion is here because using Zero Sized Types requires
-        // special handling which hasn't been implemented yet.
-        assert!(std::mem::size_of::<K>() != 0, "We're not ready to handle ZSTs");
+  pub fn new(controller: Box<HeightControl<K>>) -> SkipList<K> {
+    // This assertion is here because using Zero Sized Types requires
+    // special handling which hasn't been implemented yet.
+    assert!(std::mem::size_of::<K>() != 0, "We're not ready to handle ZSTs");
 
-        SkipList {
-            head_: Box::into_raw(Box::new(Node::new(Default::default(), controller.max_height()))),
-            length_: 0,
-            height_: 0,
-            controller_: controller,
-        }
+    SkipList {
+      // This is the ghost node mentioned above. The fact that we need a dummy
+      // variable here is the reason we have a Default constraint on K.
+      head_: Box::into_raw(Box::new(Node::new(Default::default(), controller.max_height()))),
+      length_: 0,
+      height_: 0,
+      // See comment on `SkipList::max_height` for reference.
+      max_height_: controller.max_height(),
+      // The only direct call to controller_ should be done in the
+      // `SkipList::insert` function.
+      controller_: controller,
     }
+  }
 
 
-    #[inline(always)]
-    pub fn clear(&mut self) {
-        // TODO: reimplement...
-        //*self = Self::new(self.controller_);
-    }
+  #[inline(always)]
+  pub fn clear(&mut self) {
+    // TODO: reimplement...
+    //*self = Self::new(self.controller_);
+  }
 }
 
 impl<K> SkipList<K> {
-    #[inline(always)]
-    pub fn len(&self) -> usize {
-        self.length_
-    }
+  /// Returns the number of values stored in the structure.
+  #[inline(always)]
+  pub fn len(&self) -> usize {
+    self.length_
+  }
 
-    #[inline(always)]
-    pub fn is_empty(&self) -> bool {
-        self.length_ == 0
-    }
+  /// Returns `true` if there are no values stored within the structure.
+  #[inline(always)]
+  pub fn is_empty(&self) -> bool {
+    self.length_ == 0
+  }
+
+  /// Returns the maximum reachable height of the SkipList.
+  #[inline(always)]
+  fn max_height(&self) -> usize {
+    self.max_height_
+  }
 }
 
 impl<K: Ord> SkipList<K> {
-    // Finds the node previous to the node that would have `key`, if any.
-    //
-    // This function breaks the mutability correctness, because it takes a const
-    // reference to self and returns mutable nodes.
-    pub(crate) unsafe fn find_lower_bound(&self, key: &K) -> &mut Node<K> {
-        let mut current = self.head_;
-        for height in (0..self.height_).rev() {
-            while (*current).has_next(height) &&
-                  (*current).next(height).key() < key {
-                current = (*current).mut_ptr_next(height);
-            }
-        }
-
-        &mut *current
+  /// Finds the node previous to the node that would have `key`, if any.
+  ///
+  /// This function breaks the mutability correctness, because it takes a const
+  /// reference to self and returns mutable nodes.
+  pub(crate) unsafe fn find_lower_bound(&self, key: &K) -> &mut Node<K> {
+    let mut current = self.head_;
+    for height in (0..self.height_).rev() {
+      while (*current).has_next(height) &&
+      (*current).next(height).key() < key {
+        current = (*current).mut_ptr_next(height);
+      }
     }
 
-    // Finds the node previous to the node that would have `key`, if any. It
-    // also generates an `updates` vector; the vector contains for index i, the
-    // last previous node that had height greater or equal than i.
-    //
-    // This function breaks the mutability correctness, because it takes a const
-    // reference to self and returns mutable nodes.
-    pub(crate) unsafe fn find_lower_bound_with_updates(&self, key: &K)
-      -> (&mut Node<K>, Vec<&mut Node<K>>) {
-        let max_height = self.controller_.max_height();
-        let mut updates = Vec::with_capacity(max_height);
-        // Initialization for the `updates` vector starts from the back and
-        // moves into the front. We set the length of the uninitialized
-        // vector to the actual value we are going to use, so that we can do
-        // this initialization efficiently
-        updates.set_len(max_height);
-        for height in self.height_..max_height {
-            updates[height] = &mut *self.head_;
-        }
+    &mut *current
+  }
 
-        let mut current = self.head_;
-        for height in (0..self.height_).rev() {
-            while (*current).has_next(height) &&
-                  (*current).next(height).key() < key {
-                current = (*current).mut_ptr_next(height);
-            }
-
-            updates[height] = &mut *current;
-        }
-
-        (&mut *current, updates)
+  /// Finds the node previous to the node that would have `key`, if any. It
+  /// also generates an `updates` vector; the vector contains for index i, the
+  /// last previous node that had height greater or equal than i.
+  ///
+  /// This function breaks the mutability correctness, because it takes a const
+  /// reference to self and returns mutable nodes.
+  pub(crate) unsafe fn find_lower_bound_with_updates(&self, key: &K)
+  -> (&mut Node<K>, Vec<&mut Node<K>>) {
+    let max_height = self.max_height();
+    let mut updates = Vec::with_capacity(max_height);
+    // Initialization for the `updates` vector starts from the back and
+    // moves into the front. We set the length of the uninitialized
+    // vector to the actual value we are going to use, so that we can do
+    // this initialization efficiently
+    updates.set_len(max_height);
+    for height in self.height_..max_height {
+      updates[height] = &mut *self.head_;
     }
 
-    pub fn insert(&mut self, key: K) -> bool {
-        // TODO: initialize this later. This may not ever get used if the key
-        // already exists
-        let height = self.controller_.get_height(&key);
+    let mut current = self.head_;
+    for height in (0..self.height_).rev() {
+      while (*current).has_next(height) &&
+      (*current).next(height).key() < key {
+        current = (*current).mut_ptr_next(height);
+      }
 
-        unsafe {
-            let (current, mut updates)
-                = self.find_lower_bound_with_updates(&key);
-
-            // The lower bound's next node, if present, could be the same as the
-            // key we are looking for, so we could abort early here
-            if current.has_next(0) {
-                if current.next(0).key() == &key {
-                    return false;
-                }
-            }
-
-            // Generate the node. All memory allocation is done using Box so
-            // that we can actually free it using Box later
-            let node = Box::into_raw(Box::new(Node::new(key, height)));
-            for h in 0..std::cmp::max(height, 1) {
-                (*node).set_next(h, updates[h].mut_ptr_next(h));
-                updates[h].set_next(h, node);
-            }
-        }
-
-        self.height_ = std::cmp::max(self.height_, height);
-        self.length_ += 1;
-        true
+      updates[height] = &mut *current;
     }
 
-    pub fn get(&self, key: &K) -> Option<&K> {
-        let node : &Node<K> = unsafe { self.find_lower_bound(key) };
-        match node.next_or(0) {
-            None => None,
-            Some(next) => {
-                if next.key() == key {
-                    Some(next.key())
-                } else {
-                    None
-                }
-            }
+    (&mut *current, updates)
+  }
+
+  pub fn insert(&mut self, key: K) -> bool {
+    // TODO: initialize this later. This may not ever get used if the key
+    // already exists
+    let height = self.controller_.get_height(&key);
+
+    unsafe {
+      let (current, mut updates)
+      = self.find_lower_bound_with_updates(&key);
+
+      // The lower bound's next node, if present, could be the same as the
+      // key we are looking for, so we could abort early here
+      if current.has_next(0) {
+        if current.next(0).key() == &key {
+          return false;
         }
+      }
+
+      // Generate the node. All memory allocation is done using Box so
+      // that we can actually free it using Box later
+      let node = Box::into_raw(Box::new(Node::new(key, height)));
+      for h in 0..std::cmp::max(height, 1) {
+        (*node).set_next(h, updates[h].mut_ptr_next(h));
+        updates[h].set_next(h, node);
+      }
     }
 
-    #[inline(always)]
-    pub fn contains(&self, key: &K) -> bool {
-        self.get(key).is_some()
+    self.height_ = std::cmp::max(self.height_, height);
+    self.length_ += 1;
+    true
+  }
+
+  pub fn get(&self, key: &K) -> Option<&K> {
+    let node : &Node<K> = unsafe { self.find_lower_bound(key) };
+    match node.next_or(0) {
+      None => None,
+      Some(next) => {
+        if next.key() == key {
+          Some(next.key())
+        } else {
+          None
+        }
+      }
+    }
+  }
+
+  #[inline(always)]
+  pub fn contains(&self, key: &K) -> bool {
+    self.get(key).is_some()
+  }
+
+  pub fn remove(&mut self, key: &K) -> bool {
+    unsafe {
+      let (current, mut updates) = self.find_lower_bound_with_updates(&key);
+
+      // 'current' is the lower bound to the node, so if it doesn't have a
+      // next node at level 0, it means that 'key' is not present. If it
+      // does exist, then there is a possibility that it may be greater
+      // than the actual key we are looking for
+      if !current.has_next(0) {
+        return false
+      }
+
+      let next = current.mut_ptr_next(0);
+      // If the key is not the one that we are looking for, then that
+      // means we are done too
+      if (*next).key() != key {
+        return false
+      }
+
+      for h in 0..std::cmp::max((*next).height(), 1) {
+        updates[h].set_next(h, (*next).mut_ptr_next(h));
+      }
+
+      // Free the memory for the 'next' pointer
+      Box::from_raw(next);
     }
 
-    pub fn remove(&mut self, key: &K) -> bool {
-        unsafe {
-            let (current, mut updates)
-                = self.find_lower_bound_with_updates(&key);
+    // Update length
+    self.length_ -= 1;
+    return true
+  }
 
-            // 'current' is the lower bound to the node, so if it doesn't have a
-            // next node at level 0, it means that 'key' is not present. If it
-            // does exist, then there is a possibility that it may be greater
-            // than the actual key we are looking for
-            if !current.has_next(0) {
-                return false
-            }
+  pub fn replace(&mut self, key: K) -> Option<K> {
+    let current = unsafe { self.find_lower_bound(&key) };
 
-            let next = current.mut_ptr_next(0);
-            // If the key is not the one that we are looking for, then that
-            // means we are done too
-            if (*next).key() != key {
-                return false
-            }
-
-            for h in 0..std::cmp::max((*next).height(), 1) {
-                updates[h].set_next(h, (*next).mut_ptr_next(h));
-            }
-
-            // Free the memory for the 'next' pointer
-            Box::from_raw(next);
-        }
-
-        // Update length
-        self.length_ -= 1;
-        return true
+    // 'current' is the lower bound to the node, so if it doesn't have a
+    // next node at level 0, it means that 'key' is not present. If it
+    // does exist, then there is a possibility that it may be greater
+    // than the actual key we are looking for
+    if !current.has_next(0) {
+      return None
     }
 
-    pub fn replace(&mut self, key: K) -> Option<K> {
-        let current = unsafe { self.find_lower_bound(&key) };
-
-        // 'current' is the lower bound to the node, so if it doesn't have a
-        // next node at level 0, it means that 'key' is not present. If it
-        // does exist, then there is a possibility that it may be greater
-        // than the actual key we are looking for
-        if !current.has_next(0) {
-            return None
-        }
-
-        let next = current.mut_next(0);
-        // If the key is not the one that we are looking for, then that
-        // means we are done too
-        if next.key() != &key {
-            return None
-        }
-
-        return Some(next.replace_key(key))
+    let next = current.mut_next(0);
+    // If the key is not the one that we are looking for, then that
+    // means we are done too
+    if next.key() != &key {
+      return None
     }
+
+    return Some(next.replace_key(key))
+  }
 }
 
 impl<K: std::fmt::Display> std::fmt::Display for SkipList<K> {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "[ ").unwrap();
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "[ ").unwrap();
 
-        for key in self.iter() {
-            write!(f, "{} ", key).unwrap();
-        }
-
-        write!(f, "]").unwrap();
-        std::result::Result::Ok(())
+    for key in self.iter() {
+      write!(f, "{} ", key).unwrap();
     }
+
+    write!(f, "]").unwrap();
+    std::result::Result::Ok(())
+  }
 
 }
 
 impl<K> Drop for SkipList<K> {
-    fn drop(&mut self) {
-        unsafe {
-            let mut current = self.head_;
-            while (*current).has_next(0) {
-                let next = (*current).mut_ptr_next(0);
-                Box::from_raw(current);
-                current = next;
-            }
-            Box::from_raw(current);
-        }
+  fn drop(&mut self) {
+    unsafe {
+      let mut current = self.head_;
+      while (*current).has_next(0) {
+        let next = (*current).mut_ptr_next(0);
+        Box::from_raw(current);
+        current = next;
+      }
+      Box::from_raw(current);
     }
+  }
 }
 
 impl<K: Ord> std::ops::Index<K> for SkipList<K> {
-    type Output = K;
+  type Output = K;
 
-    #[inline(always)]
-    fn index(&self, index: K) -> &Self::Output {
-        return self.get(&index).unwrap();
-    }
+  #[inline(always)]
+  fn index(&self, index: K) -> &Self::Output {
+    return self.get(&index).unwrap();
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn new_empty() {
-        // let k_upgrade_probability = 0.5;
-        // let k_max_height = 3;
-        // let list : SkipList<u32> =
-        //     SkipList::new(k_upgrade_probability, k_max_height);
-        // assert_eq!(list.len(), 0);
-        // assert_eq!(list.upgrade_probability(), k_upgrade_probability);
-        // assert_eq!(list.max_height(), k_max_height);
-        // assert!(list.is_empty());
+  #[test]
+  fn new() {
+    let list : SkipList<i32> = Default::default();
+    assert_eq!(list.len(), 0);
+    assert!(list.is_empty());
+  }
+
+  #[test]
+  fn insert_get_single() {
+    let k_key = 34;
+    let mut list : SkipList<i32> = Default::default();
+    assert!(list.insert(k_key));
+
+    assert_eq!(list.len(), 1);
+    assert!(!list.is_empty());
+
+    let fetched = list.get(&k_key);
+    assert!(fetched.is_some());
+    assert_eq!(*fetched.unwrap(), k_key);
+
+    let second_fetched = list.get(&k_key);
+    assert!(second_fetched.is_some());
+    // The keys returned in multiple get() calls should always point to the same
+    // address as the first one (there should be no copies).
+    assert_eq!(second_fetched.unwrap(), fetched.unwrap());    
+  }
+
+  #[test]
+  fn insert_get_duplicate() {
+    let k_key = 55;
+    let mut list : SkipList<i32> = Default::default();
+
+    assert!(list.insert(k_key));
+
+    {
+      let first_fetched = list.get(&k_key);
+      assert!(first_fetched.is_some());
+      // This is value comparison. The key should be the same as the one inserted
+      assert_eq!(*first_fetched.unwrap(), k_key);
     }
 
-    #[test]
-    fn insert_single_clear() {
-        // let k_upgrade_probability = 0.7;
-        // let k_max_height = 8;
-        // let mut list : SkipList<i32> =
-        //     SkipList::new(k_upgrade_probability, k_max_height);
-        // assert!(list.insert(34));
-        // assert_eq!(list.len(), 1);
-        // assert!(!list.is_empty());
-
-        // list.clear();
-        // assert_eq!(list.len(), 0);
-        // assert_eq!(list.upgrade_probability(), k_upgrade_probability);
-        // assert_eq!(list.max_height(), k_max_height);
-        // assert!(list.is_empty());
-    }
-
-    #[test]
-    fn insert_already_exists() {
-
-    }
-
-    #[test]
-    fn insert_multiple() {
-
-    }
-
-    #[test]
-    fn get_single() {
-
-    }
-
-    #[test]
-    fn get_multiple() {
-
-    }
-
-    #[test]
-    fn get_not_found() {
-
-    }
-
-    #[test]
-    fn remove_from_empty() {
-
-    }
-
-    #[test]
-    fn remove_not_found() {
-
-    }
-
-    #[test]
-    fn remove_from_single() {
-
-    }
-
-    #[test]
-    fn remove_from_multiple() {
-
-    }
-
-    #[test]
-    fn replace_from_empty() {
-
-    }
-
-    #[test]
-    fn replace_not_found() {
-
-    }
-
-    #[test]
-    fn replace_from_single() {
-
-    }
-
-    #[test]
-    fn replace_from_multiple() {
-
-    }
+    // The second insertion should fail, the key is already there
+    assert!(!list.insert(k_key));
+    // Duplicate insertions don't change the length
+    assert_eq!(list.len(), 1);
+    let second_fetched = list.get(&k_key);
+    assert!(second_fetched.is_some());
+    // This is reference comparison. The reference returned should be the same
+    // as the reference returned the first time (i.e. there should be no new
+    // key allocations)
+    // TODO: this has problems due to lifetimes. 
+    // assert_eq!(first_fetched.unwrap(), second_fetched.unwrap());
+  }
 }
